74630e2e81862f398a2439623771fc132f3ddfbe
ntua-el19092 <el19092@mail.ntua.gr>
2023-11-22 22:10:02 +0200

    test

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,7 +1,9 @@
 const express = require('express');
 router = express.Router()
 const healthcheck = require('../controllers/healthcheck')
+const uploadtsv = require('../controllers/uploadtsv')
 
 router.get('/healthcheck', healthcheck)
+router.post('/upload/titlebasics', uploadtsv)
 
 module.exports = router
