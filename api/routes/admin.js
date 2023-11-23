48e9fc107a5a77ca14ab4c41801462528a331abe
ntua-el19092 <el19092@mail.ntua.gr>
2023-11-23 11:52:12 +0200

    added title basics and title akas uploads

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,9 +1,13 @@
 const express = require('express');
 router = express.Router()
 const healthcheck = require('../controllers/healthcheck')
-const uploadtsv = require('../controllers/uploadtsv')
+const uploadtb = require('../controllers/uploadtb')
+const uploadakas = require('../controllers/uploadakas')
+
 
 router.get('/healthcheck', healthcheck)
-router.post('/upload/titlebasics', uploadtsv)
+router.post('/upload/titlebasics', uploadtb)
+router.post('/upload/titleakas', uploadakas)
+
 
 module.exports = router
