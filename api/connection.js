0be9f417a21595c22b3b12faf7c46620bd62252a
ntua-el19063 <el19063@mail.ntua.gr>
2024-01-04 19:19:31 +0200

    first

--- api/connection.js
+++ api/connection.js
@@ -5,7 +5,8 @@ const connection = mysql2.createConnection({
     host: "localhost",
     database: "ntuaflix",
     user: "root",
-    password : ""
+    password : "",
+    port: '3307'
 })
 
 module.exports = connection
